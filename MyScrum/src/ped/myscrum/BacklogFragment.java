2b3d26dd8a137ce67e410e4e69d1b7e07d5c13b1
Castandet <flo.dorient@hotmail.fr>
2014-03-11 15:36:40 +0100

    Added files

--- MyScrum/src/ped/myscrum/BacklogFragment.java
+++ MyScrum/src/ped/myscrum/BacklogFragment.java
@@ -1,19 +1,28 @@
 package ped.myscrum;
 
 import info.androidhive.slidingmenu.R;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import ped.myscrum.adapter.ExpandableListAdapter;
 import android.app.Fragment;
-import android.os.Bundle;
 import android.app.FragmentManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
 
 public class BacklogFragment extends Fragment {
@@ -22,50 +31,130 @@ public class BacklogFragment extends Fragment {
 	ExpandableListView expListView;
 	List<String> listDataHeader;
 	HashMap<String, List<String>> listDataChild;
+	private CharSequence api_key;
+	private int project_id;
 
 	@Override
 	 public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	            Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		View rootView = inflater.inflate(R.layout.fragment_backlog, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_team, container, false);
 
 		expListView = (ExpandableListView) rootView.findViewById(R.id.lvExp);
-		prepareListData();
-		listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-		expListView.setAdapter(listAdapter);
+		api_key = getArguments().getString("api_key");
+		project_id = getArguments().getInt("project_id");
+		
+		listDataHeader = new ArrayList<String>();
+		listDataChild = new HashMap<String, List<String>>();
+		
+		new TeamInformationRetrieval(listDataHeader, listDataChild, expListView).execute("http://10.0.2.2:3000/api/owner/projects/" + project_id + "/user_stories?api_key=" + api_key);
+		
 		expListView.setOnGroupClickListener(new OnGroupClickListener() {
+
 			@Override
 			public boolean onGroupClick(ExpandableListView parent, View v,
 					int groupPosition, long id) {
-				if(listDataHeader.get(groupPosition).equals("Create User Story")){
-					Fragment fragment = new CreateUserStoryFragment();
-					FragmentManager fragmentManager = getFragmentManager();
-					fragmentManager.beginTransaction()
-							.replace(R.id.frame_container, fragment).commit();
+				return false;
+			}
+		});
+
+
+
+		expListView.setOnChildClickListener(new OnChildClickListener() {
+
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v,
+					int groupPosition, int childPosition, long id) {
+				switch (childPosition) {
+					case 0:
+						break;
+					case 1:
+						break;		
+					default:
+						break;
 				}
 				return false;
 			}
 		});
-		
 		return rootView;
 	}
 
-	private void prepareListData() {
-		listDataHeader = new ArrayList<String>();
-		listDataChild = new HashMap<String, List<String>>();
+	
 
-		listDataHeader.add("User Story #1");
-		listDataHeader.add("Create User Story");
-
-		List<String> userStory1 = new ArrayList<String>();
-		userStory1.add("Title");
-		userStory1.add("Description");
-		userStory1.add("Priority");
-		userStory1.add("Difficulty");
-		userStory1.add("Finished");
-		userStory1.add("Validated");
+	private class TeamInformationRetrieval extends AsyncTask<String, String, String>{
 		
-		listDataChild.put(listDataHeader.get(0), userStory1);
+		private List<String> listDataHeader;
+		private HashMap<String, List<String>> listDataChild;
+		ExpandableListAdapter listAdapter;
+		ExpandableListView expListView;
+
+
+		
+		public TeamInformationRetrieval(List<String> listHeader, HashMap<String, List<String>> listChild, ExpandableListView listView){
+			listDataHeader = listHeader;
+			listDataChild = listChild;
+			expListView = listView;
+
+		}
+		
+		
+		@Override
+		protected String doInBackground(String... url){
+			String result = " ";
+			URL url_init;
+			HttpURLConnection conn;
+			BufferedReader rd;
+			String line;
+			try{
+			url_init = new URL(url[0]);
+			conn = (HttpURLConnection) url_init.openConnection();
+			conn.setRequestMethod("GET");
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			while ((line = rd.readLine()) != null) {
+				result += line;
+			}
+			rd.close();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+		
+		
+		@Override
+	    protected void onPostExecute(String result) {
+	        
+			super.onPostExecute(result);
+	      
+			try{
+			JSONArray data;
+			data = new JSONArray(result);
+			
+			for(int i=0; i<data.length(); i++)
+				listDataHeader.add("User Story #" + data.getJSONObject(i).getString("id"));
+		
+			for(int i=0; i< listDataHeader.size(); i++){
+				List<String> project = new ArrayList<String>();
+				project.add(data.getJSONObject(i).getString("title"));
+				project.add(data.getJSONObject(i).getString("description"));
+				project.add("Priority: " + data.getJSONObject(i).getString("priority"));
+				project.add("Difficulty: " + data.getJSONObject(i).getString("difficulty"));
+				project.add("Finished: " + data.getJSONObject(i).getString("finished"));
+				project.add("Validated: " + data.getJSONObject(i).getString("valid"));
+				listDataChild.put(listDataHeader.get(i), project);
+			}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+
+			listAdapter = new ExpandableListAdapter(BacklogFragment.this, listDataHeader, listDataChild);
+			this.expListView.setAdapter(listAdapter);
+			
+	    }
+		
+		
+		
 
 	}
 }
